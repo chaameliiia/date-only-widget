de606b48e18d94431ba82670bc34f5a9f1741156
chaameliiia <chaameliiia@gmail.com>
2022-12-28 20:16:44 +0900

    feature : pexel api 연결

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
 	import { date } from '../stores';
-  import '../styles/style.css';
+	import '../styles/style.css';
+
+	/** @type {import('./$types').PageData} */
+
+	export let data;
+	export let photo = data.photos?.[0];
 </script>
 
 <p>{$date}</p>
+<img src={photo?.url || ''} alt={photo?.alt || ''} />
 
 <style src="../styles/style.css"></style>
