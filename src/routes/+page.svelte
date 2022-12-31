db22c87c923d8754d950ec88766581b24a7b6040
chaameliiia <chaameliiia@gmail.com>
2023-01-01 05:19:52 +0900

    style : 날짜 표기 방법 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
-<script>
-	import { date } from '../stores';
+<script lang="ts">
+	import type { IData } from '$lib/types';
+	import { getCompColor } from '$lib/utils/getCompColor';
+	import { year, month, date, days } from '../stores';
 	import '../styles/style.css';
 
 	/** @type {import('./$types').PageData} */
 
-	export let data;
-	export let photo = data.photos?.[0];
+	export let data: IData;
+	export let src = data.photos[0].src.original;
+	export let color = getCompColor(data.photos[0].avg_color.slice(1));
 </script>
 
-<p>{$date}</p>
-<img src={photo?.src.large || ''} alt={photo?.alt || ''} />
+<div class="wrapper" style="--url: url({src})">
+	<p class="month" style="--color: {color}">
+		{$month}
+	</p>
+	<div class="col_wrapper">
+		<p class="year">{$year}</p>
+		<div class="row_wrapper">
+			<p class="date">{$date}</p>
+			<p class="days">{$days}</p>
+		</div>
+	</div>
+</div>
 
 <style src="../styles/style.css"></style>
--- src/stores.ts
+++ src/stores.ts
@@ -1,12 +1,33 @@
 import { readable } from 'svelte/store';
 
-export const date = readable('', (set) => {
-	const daysArr = ['일', '월', '화', '수', '목', '금', '토'];
-	const now = new Date();
-	const year = now.getFullYear();
-	const month = now.getMonth() + 1;
-	const date = now.getDate();
-	const days = daysArr[now.getDay()];
+const now = new Date();
 
-	set(`${year}년 ${month}월 ${date}일 ${days}요일`);
+export const year = readable('', (set) => set(`${now.getFullYear()}`));
+
+export const month = readable('', (set) => {
+	const monthArr = [
+		'JAN',
+		'FEB',
+		'MAR',
+		'APR',
+		'MAY',
+		'JUN',
+		'JUL',
+		'AUG',
+		'SEP',
+		'OCT',
+		'NOV',
+		'DEC'
+	];
+
+	set(monthArr[now.getMonth()]);
+});
+
+export const date = readable(0, (set) => {
+	set(now.getDate());
+});
+
+export const days = readable('', (set) => {
+	const daysArr = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', ''];
+	set(daysArr[now.getDay()]);
 });
